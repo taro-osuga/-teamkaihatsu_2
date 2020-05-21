09a2d84999533f335bc049dbd6fbae234e0f6824
taroosuga <copernix0@gmail.com>
2020-05-21 11:41:25 +0900

    user削除機能の権限実装

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,7 @@
 class TeamsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_team, only: %i[show edit update destroy]
+  before_action :cannot_edit_unowner, only: %i[edit update]
 
   def index
     @teams = Team.all
@@ -55,5 +56,11 @@ class TeamsController < ApplicationController
 
   def team_params
     params.fetch(:team, {}).permit %i[name icon icon_cache owner_id keep_team_id]
+  end
+
+  def cannot_edit_unowner
+    unless current_user == @team.owner
+      redirect_to @team, notice: I18n.t('views.messages.no_authority')
+    end
   end
 end
